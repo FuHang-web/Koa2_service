d9369b6a1349a75c34238aaa66c54e1cadccc1cd
FuHang <15223184837@163.com>
2021-10-28 22:50:54 +0800

    update

--- middleware/OSSMiddleware.js
+++ middleware/OSSMiddleware.js
@@ -25,17 +25,6 @@ const clientConfigure = async (ctx, next) => {
   }
   return client
 }
-// let client = new OSS()
-// let client = new OSS({
-//   // yourRegion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
-//   region: 'oss-cn-chengdu',
-//   // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//   accessKeyId: 'LTAI5tD9h5qMvMJxML2NDCEx',
-//   accessKeySecret: 'Gf3spX0IGKcyhJX65zYUicbu9utjsp',
-//   // 填写Bucket名称。
-//   bucket: 'fu-cloud'
-// });
-
 
 const putStream = async (ctx, next) => {
   try {
